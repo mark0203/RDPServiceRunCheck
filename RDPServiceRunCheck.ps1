fe64938175e3bcb23b83a87a9fa5092d6c7a5ef2
Mark Duivesteijn <DuivesteijnM@Mproof.local>
2017-12-20 14:27:46 +0100

    Uploaded the code for the powershell script v0.1

---
+++ RDPServiceRunCheck.ps1
@@ -0,0 +1,43 @@
+﻿Param
+(
+    [Parameter(Mandatory=$True)]
+    [string]$pwd,
+    [Parameter(Mandatory=$True)]
+    [string]$cname,       
+    [Parameter(Mandatory=$True)]
+    [string]$uname
+)
+
+$password = $pwd | ConvertTo-SecureString -asPlainText -Force
+$remotingCredential = New-Object System.Management.Automation.PSCredential("$cname\$uname",$password)
+
+$session = $null
+$i = 0
+$done = $false
+$endDate = (Get-Date) + "00:15:00"
+Write-Host "Connection loop will end at $endDate."
+while ($done -eq $false -and (Get-Date) -lt $endDate)
+{
+    $i = $i + 1
+    try
+    {        
+        $now = Get-Date
+    `	Write-Host "[$now] Attempting to connect to $cname. Try: $i."
+        $session = New-PSSession -ComputerName $cname -Credential $remotingCredential -UseSSL -Authentication Negotiate -ErrorAction Stop
+    	Write-Host "Connected to $cname."
+
+        Invoke-Command -Session $session -ErrorAction Stop -ScriptBlock {
+            Write-Host "Checking if TermService is running..."
+            $svc = Get-Service TermService -ErrorAction Stop
+            $svc.WaitForStatus('Running', "00:00:30")
+            Write-Host "TermService is running."
+        }
+        $done = $true
+    }
+    catch 
+    {
+        $ErrorMessage = $_.Exception.Message
+        Write-Host "Error: $ErrorMessage"
+    }
+}
+
